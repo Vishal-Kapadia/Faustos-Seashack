d9e45271df314952cc3f0ac6c3b4a626385efc5d
Vishal-Kapadia <vishalkapadia927@gmail.com>
2024-04-23 22:08:53 -0700

    Added code to be tested for home

--- 561Project/frontend/src/pages/Home.jsx
+++ 561Project/frontend/src/pages/Home.jsx
@@ -1,7 +1,5 @@
 //import './App.css';
 import NavbarGo from '../components/Navbar'
-import EntreesSection from '../components/Entrees'
-import EntreesBoxes from '../components/Boxes'
 import Footerbox from '../components/Footer'
 //import Note from "../components/Note"
 
@@ -12,8 +10,71 @@ function Home() {
   return (
     <div>
       <NavbarGo />
-      <EntreesSection/>
-      <EntreesBoxes/>
+      
+      <section className="bg-dark text-primary p-4 text-center">
+        <div className="container">
+            <div>
+                <div>
+                    <h1>Welcome to Fausto's Seashack!</h1>
+                    <h4>Check out what we have to offer below!</h4>
+                </div>
+            </div>
+        </div>
+      </section>
+
+      <section className="p-4">
+        <div className="container-fluid">
+            <div className="row text-center">
+                <div className="col-md">
+                    <div className="card bg-light text-dark pb-2">
+                        <div className="card-body text-center">
+                            <h1 className="card-title mb-3 text-warning">
+                                Daily Discounts
+                            </h1>
+                            <p className="card-text mb-3">
+                                This will list all the items that are discounted.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md">
+                    <div className="card bg-light text-dark pb-2">
+                        <div className="card-body text-center">
+                            <h1 className="card-title mb-3 text-warning text-wrap">
+                                Promotional Items
+                            </h1>
+                            <p className="card-text">
+                                This will list all the items that we think customers should try out this season.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md">
+                    <div className="card bg-light text-dark pb-2">
+                        <div className="card-body text-center">
+                            <h1 className="card-title mb-3 text-warning">
+                                Hours of Operation
+                            </h1>
+                            <p className="card-text">
+                                <ul className="text-dark text-wrap">
+                                    <li className="pb-4">
+                                        Monday thru Thursday: 9AM to 8PM
+                                    </li>
+                                    <li className="pb-4">
+                                        Friday: 9AM to 6PM
+                                    </li>
+                                    <li className="mb-2">
+                                        Saturday thru Sunday: <span className="text-warning">CLOSED</span>
+                                    </li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
+
       <Footerbox />
     </div>
   );
