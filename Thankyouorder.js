aaf4df084448a12b3fd7d92bc9afe23da8c172a3
FaustoSaavedraGonzalez <156982777+FaustoSaavedraGonzalez@users.noreply.github.com>
2024-04-24 00:39:52 -0700

    Add files via upload

---
+++ Thankyouorder.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>
+       <div class="container">
+    <div class="row">
+      <div class="col-md-8 offset-md-2 overlay">
+  
+        <h1 class="heading">Thank You for Your Order!</h1>
+        <p class="message">We hope you enjoy your meal!</p>
+      </div>
+    </div>
+  </div>
+
+        </p> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
