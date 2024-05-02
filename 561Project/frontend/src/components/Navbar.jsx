0949226b42ad7beccc2af6c025ffaea09805ab9d
Vishal-Kapadia <vishalkapadia927@gmail.com>
2024-05-01 17:57:51 -0700

    changed navbar

--- 561Project/frontend/src/components/Navbar.jsx
+++ 561Project/frontend/src/components/Navbar.jsx
@@ -17,19 +17,19 @@ const NavbarGo = () => {
   return (
     <Navbar expand="lg" bg="light">
       <Container fluid>
-        <Navbar.Brand href="#Homepage" className="text-primary">Fausto's Seashack</Navbar.Brand>
+        <Navbar.Brand href="#Homepage" className="text-dark">Fausto's Seashack</Navbar.Brand>
         <Navbar.Toggle aria-controls="navmenu" />
         <Navbar.Collapse id="navmenu">
           <Nav className="ms-auto">
-            <NavLink to="/home" className="nav-link text-primary" activeClassName="active">Home</NavLink>
-            <NavLink to="/entrees" className="nav-link text-primary" activeClassName="active">Entrees</NavLink>
-            <NavLink to="/specialties" className="nav-link text-primary" activeClassName="active">Specialties</NavLink>
-            <NavLink to="/deals" className="nav-link text-primary" activeClassName="active">Deals</NavLink>
-            <NavLink to="/about" className="nav-link text-primary" activeClassName="active">About</NavLink>
+            <NavLink to="/home" className="nav-link text-dark" activeClassName="active">Home</NavLink>
+            <NavLink to="/entrees" className="nav-link text-dark" activeClassName="active">Entrees</NavLink>
+            <NavLink to="/specialties" className="nav-link text-dark" activeClassName="active">Specialties</NavLink>
+            <NavLink to="/deals" className="nav-link text-dark" activeClassName="active">Deals</NavLink>
+            <NavLink to="/about" className="nav-link text-dark" activeClassName="active">About</NavLink>
             {!isLoggedIn && (
               <>
                 <NavLink to="/register" className="nav-link text-primary" activeClassName="activee">Create Account</NavLink>
-                <NavLink to="/login" className="nav-link text-primary" activeClassName="activee">Sign In</NavLink>
+                <NavLink to="/login" className="nav-link text-dark" activeClassName="activee">Sign In</NavLink>
               </>
             )}
             {isLoggedIn && (
