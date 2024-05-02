c770e02dc4f3bc87d00e84af123ec21f634e9604
Nick-Swany <nickswanson.nps@gmail.com>
2024-05-01 22:37:27 -0700

    smal syntex changes

--- 561Project/frontend/src/components/ItemsButtons.jsx
+++ 561Project/frontend/src/components/ItemsButtons.jsx
@@ -11,7 +11,7 @@ const ItemsButtons = ({ foodItem, addToCart }) => {
             <Card size="md" className="bg-dark text-light" style={{ height: '100%' }}>
                 <Card.Img variant="top" src={foodItem.image} size="sm" />
                 <Card.Body className="text-center d-flex flex-column" style={{ height: '100%' }}>
-                    <h5 className="mb-0">{foodItem.name}</h5> {/* Remove margin-bottom for the title */}
+                    <h5 className="mb-0">{foodItem.name}</h5> 
                     <p className="text-muted">{foodItem.description}</p> {/* Add a class for description styling */}
                     <div className="mt-auto">
                         <Button variant="primary" className="bg-danger" size="sm" onClick={handleClick}>Add to Cart</Button>
