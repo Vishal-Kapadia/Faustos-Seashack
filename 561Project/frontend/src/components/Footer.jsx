ac300492d34a8cd46b2b467a77b10a87f4d93f46
Fausto Saavedra Gonzalez <faustos44911@gmail.com>
2024-05-01 18:40:19 -0700

    added footer

---
+++ 561Project/frontend/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-content">
+          <p>&copy; 2024 Fausto's Seashack. All rights reserved.</p>
+          <ul className="footer-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
