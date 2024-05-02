bed9eb887399c32faefe625458f8c9b94079e901
Nick-Swany <nickswanson.nps@gmail.com>
2024-05-01 21:44:42 -0700

    Fixed error where cart was convered by footer

--- 561Project/frontend/src/components/AddToCart.jsx
+++ 561Project/frontend/src/components/AddToCart.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Col, Card, Button } from 'react-bootstrap';
+import { Col, Card } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 
 const Cart = () => {
   return (
     <Col md className="my-2">
-      <Card className="bg-dark text-light">
+      <Card className="bg-dark text-light" style={{ marginBottom: '50px' }}>
         <Card.Body className="text-center">
           <NavLink to="/mycart" className="nav-link">
             Cart
