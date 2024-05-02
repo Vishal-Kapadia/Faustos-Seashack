f311e4cd0b25e02a80495438e47e6d6ffbe7bcfe
Vishal-Kapadia <vishalkapadia927@gmail.com>
2024-05-01 21:30:29 -0700

    more styling to homepage

--- 561Project/frontend/src/components/ThreeBoxes.jsx
+++ 561Project/frontend/src/components/ThreeBoxes.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../styles/ThreeBoxes.css";
 
 const ThreeBoxes = () => {
   return (
@@ -9,10 +10,10 @@ const ThreeBoxes = () => {
             <div className="card h-100 border-warning">
               <div className="card-body text-center">
                 <h2 className="card-title text-warning mb-4">Daily Discounts</h2>
-                <ul className="list-unstyled">
-                  <li>Fire Wings - 10% off!</li>
-                  <li>Vishal's Special Spaghetti - 15% off!</li>
-                  <li>THE Burrito - 15% off!</li>
+                <ul className="list-unstyled custom-list">
+                  <li className="mb-3">Fire Wings - 10% off!</li>
+                  <li className="mb-3">Vishal's Special Spaghetti - 15% off!</li>
+                  <li className="mb-3">THE Burrito - 15% off!</li>
                 </ul>                
               </div>
             </div>
@@ -21,10 +22,10 @@ const ThreeBoxes = () => {
             <div className="card h-100 border-warning">
               <div className="card-body text-center">
                 <h2 className="card-title text-warning mb-4">Promotional Items</h2>
-                <ul className="list-unstyled">
-                  <li>The delicious Clam Chowder, found in Specialties!</li>
-                  <li>The one, the only, for a limited time... Nick's GOATED burger in Entrees!</li>
-                  <li>The Iberian Paella is here only for this summer! Don't miss out in Deals!</li>
+                <ul className="list-unstyled custom-list">
+                  <li className="mb-3">The delicious Clam Chowder, found in Specialties!</li>
+                  <li className="mb-3">The one, the only, for a limited time... Nick's GOATED burger in Entrees!</li>
+                  <li className="mb-3">The Iberian Paella is here only for this summer! Don't miss out in Deals!</li>
                 </ul>
               </div>
             </div>
@@ -33,10 +34,10 @@ const ThreeBoxes = () => {
             <div className="card h-100 border-warning">
               <div className="card-body text-center">
                 <h2 className="card-title text-warning mb-4">Hours of Operation</h2>
-                <ul className="list-unstyled">
-                  <li>Monday thru Thursday: 9AM to 8PM</li>
-                  <li>Friday thru Saturday: 9AM to 10PM</li>
-                  <li>Sunday: <span className="text-warning">CLOSED</span></li>
+                <ul className="list-unstyled custom-list">
+                  <li className="mb-3">Monday thru Thursday: 9AM to 8PM</li>
+                  <li className="mb-3">Friday thru Saturday: 9AM to 10PM</li>
+                  <li className="mb-3">Sunday: <span className="text-warning">CLOSED</span></li>
                 </ul>
               </div>
             </div>
